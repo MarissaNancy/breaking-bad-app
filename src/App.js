6bd2f1c94b652daf1485d9d4534b06dcda091b4e
Nancy Marissa Gonzalez <nancygonzalez7@yahoo.com>
2021-09-20 13:02:44 -0700

    added query for charachters

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ import './App.css';
 const App = () => {
   const [items, setItems] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [query, setQuery] = useState('')
 
   useEffect(() => {
     const fetchItems = async () => {
-      const result = await axios(`https://www.breakingbadapi.com/api/characters`)
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`)
 
       console.log(result.data)
       setItems(result.data)
@@ -19,12 +20,12 @@ const App = () => {
     }
 
     fetchItems()
-  }, [])
+  }, [query])
 
   return (
     <div className="container">
       <Header />
-      <Search />
+      <Search getQuery={(q) => setQuery(q)} />
       <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
